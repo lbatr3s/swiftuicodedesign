65e8e1763a3bbd6a6fd8c8820f7a4e8616f23c2a
Lester Batres <lester.batres@xoom.com>
2019-08-29 21:17:53 -0600

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Lester Batres on 8/29/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
